52396b2bf0613c7cc56efd4d7f3d144498423a03
Stuart Ansell <whyshouldsay@gmail.com>
2022-02-13 21:52:00 +0000

    Terraform

---
+++ azure-virtual-machine/variables.tf
@@ -0,0 +1,17 @@
+﻿variable "location" {
+  description = "Location grouping for project"
+  type = string
+  default = "ukwest"
+}
+
+variable "project" {
+  description = "Project Name used in convention"
+  type = string
+  default = "terraform"
+}
+
+variable "stage" {
+  description = "Project Name used in convention"
+  type = string
+  default = "dev"
+}
