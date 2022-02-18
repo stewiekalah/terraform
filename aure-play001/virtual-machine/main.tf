4cb70c28a167d601427155fb0b88fc384a38e518
Stuart Ansell <stuart@ansell.pw>
2022-02-18 00:46:48 +0000

    Adding a module with hopefully passed through variables

--- aure-play001/virtual-machine/main.tf
+++ aure-play001/virtual-machine/main.tf
@@ -1,5 +1,13 @@
 ﻿# Create a resource group
 resource "azurerm_resource_group" "vm" {
-  name     = "rg-${var.project}-${var.stage}"
+  name     = "rg-${var.project}-${var.environment}"
   location = var.location
+}
+
+module "vnet" {
+    source = "./vnet"
+    location = var.location
+    project = var.project
+    environment = var.environment
+    rgname = azurerm_resource_group.vm.name
 }
---
+++ aure-play001/virtual-machine/vnet/main.tf
@@ -0,0 +1,17 @@
+﻿resource "azurerm_virtual_network" "vnet" {
+  name = "vnet-${var.project}-${substr(var.location,0,3)}-${var.environment}"
+  location = var.location
+  resource_group_name = var.rgname
+  address_space = [ "192.168.128.0/18" ]
+
+}
+
+resource "azurerm_subnet" "vnet" {
+  name = "snet-vm"
+  address_prefixes = [ "192.168.128.0/24" ]
+  virtual_network_name = azurerm_virtual_network.vm.name
+  resource_group_name = var.rgname
+  depends_on = [
+    azurerm_virtual_network.vnet
+  ]
+}
