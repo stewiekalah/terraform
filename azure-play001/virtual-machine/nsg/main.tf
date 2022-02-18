bca64744be1d32327cb4b222cdb6c1f7e20408f7
Stuart Ansell <stuart@ansell.pw>
2022-02-18 23:04:13 +0000

    Moved NSG Association to separate TF File

--- azure-play001/virtual-machine/nsg/main.tf
+++ azure-play001/virtual-machine/nsg/main.tf
@@ -2,13 +2,19 @@
   name                      = "nsg-${var.nameconvention}"
   location                  = var.location
   resource_group_name       = var.rgname
+
+  # ALLOW - 80/443 Inbound
+  security_rule {
+    name                            = "Inbound-InternetTraffic"
+    source_port_range               = "*"
+    destination_port_ranges         = [ "80", "443" ]
+    protocol                        = "Tcp"
+    direction                       = "Inbound"
+    priority                        = 100
+    access                          = "Allow"
+    source_address_prefix           = "*"
+    destination_address_prefix      = "*"
+  }
+
 }
 
-resource "azurerm_subnet_network_security_group_association" "nsg-assoc" {
-  network_security_group_id = azurerm_network_security_group.nsg.id
-  subnet_id                 = var.subnet_id
-
-  depends_on = [
-    azurerm_network_security_group.nsg
-  ]
-}
---
+++ azure-play001/virtual-machine/nsg/nsg-association.tf
@@ -0,0 +1,8 @@
+﻿resource "azurerm_subnet_network_security_group_association" "nsg-assoc" {
+  network_security_group_id = azurerm_network_security_group.nsg.id
+  subnet_id                 = var.subnet_id
+
+  depends_on = [
+    azurerm_network_security_group.nsg
+  ]
+}
