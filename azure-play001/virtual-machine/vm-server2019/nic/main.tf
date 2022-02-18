edd67cc3538af82311c2bcbda18d7a37f0146c39
Stuart Ansell <stuart@ansell.pw>
2022-02-18 14:59:31 +0000

    Gonna start looking at outputs and stuff

---
+++ azure-play001/virtual-machine/vm-server2019/nic/main.tf
@@ -0,0 +1,11 @@
+﻿resource "azurerm_network_interface" "vm-server2019-nic" {
+  name                = "nic-${var.naming}"
+  location            = azurerm_resource_group.vm.location
+  resource_group_name = "${var.rgname}"
+
+  ip_configuration {
+    name                          = "nic-vm-01"
+    subnet_id                     = "${var.subnet_id}"
+    private_ip_address_allocation = "Dynamic"
+  }
+}
