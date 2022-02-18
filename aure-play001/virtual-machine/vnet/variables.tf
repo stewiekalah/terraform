daae67bea53fc3f8e95a16c32a6bd249abfa9d0f
Stuart Ansell <stuart@ansell.pw>
2022-02-18 00:50:48 +0000

    Define the variables in teh module

---
+++ aure-play001/virtual-machine/vnet/variables.tf
@@ -0,0 +1,15 @@
+﻿variable "location" {
+  description = "Define a location for Azure resources"
+}
+
+variable "project" {
+  description = "Project Name used in naming convention"
+}
+
+variable "environment" {
+  description = "Set the environment (Prod/Dev)"
+}
+
+variable "rgname" {
+  description = "resource_group_name"
+}
