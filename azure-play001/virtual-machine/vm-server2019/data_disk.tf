cc722f17e61101d8b89c3ba3f9189758d694a55b
Stuart Ansell <stuart@ansell.pw>
2022-02-18 22:34:36 +0000

    Validation Error: A managed resource "output" "vm_name" has not been declared in module.vm.

---
+++ azure-play001/virtual-machine/vm-server2019/data_disk.tf
@@ -0,0 +1,19 @@
+﻿resource "azurerm_managed_disk" "data_disk" {
+  name                  = "disk1-${azurerm_virtual_machine.vm-server2019.vm_name}"
+  location              = azurerm_virtual_machine.vm-server2019.location
+  resource_group_name   = azurerm_virtual_machine.vm-server2019.resource_group_name
+  disk_size_gb          = 60
+  storage_account_type  = "Standard_LRS"
+  create_option         = "Empty"
+}
+
+resource "azurerm_virtual_machine_data_disk_attachment" "data_disk" {
+  managed_disk_id       = azurerm_managed_disk.data_disk.id
+  virtual_machine_id    = azurerm_virtual_machine.data_disk.id
+  lun                   = "10"
+  caching               = "ReadWrite"
+
+  depends_on = [
+    azurerm_virtual_machine.vm-server2019
+  ]
+}
