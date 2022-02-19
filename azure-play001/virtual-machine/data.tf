0223667f8c3c83e5f90c40593182c864c08b3c1a
Stuart Ansell <stuart@ansell.pw>
2022-02-19 00:23:27 +0000

    Keyvault done

--- azure-play001/virtual-machine/data.tf
+++ azure-play001/virtual-machine/data.tf
@@ -1,10 +1,9 @@
 ﻿data "azurerm_key_vault" "ansellpw" {
-  name                = var.kv_name
-  resource_group_name = var.kv_rg
-
+  name                = module.keyvault.kv_name
+  resource_group_name = module.keyvault.kv_rg
 }
 
 data "azurerm_key_vault_secret" "kv_secret" {
-  name                = var.kv_secret-AdminPassword
-  key_vault_id        = data.azurerm_key_vault.ansellpw.id
+  name                = module.keyvault.kv_secret-name-AdminPassword
+  key_vault_id        = module.keyvault.kv_id
 }
--- azure-play001/virtual-machine/keyvault/outputs.tf
+++ azure-play001/virtual-machine/keyvault/outputs.tf
@@ -6,6 +6,15 @@ output "kv_name" {
     value = azurerm_key_vault.kv.name
 }
 
+output "kv_rg" {
+  value = azurerm_key_vault.kv.resource_group_name
+}
+
 output "kv_id" {
     value = azurerm_key_vault.kv.id
+}
+
+output "kv_secret-name-AdminPassword" {
+    value = azurerm_key_vault_secret.secret-AdminPassword.name
+    sensitive = true
 }
