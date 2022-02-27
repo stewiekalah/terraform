eaee2df84b67281f1c67e52ecaf11750b1c2975e
Stuart Ansell <stuart@ansell.pw>
2022-02-27 23:35:08 +0000

    Website deploys to the correct place! It works!!

--- azure-web-app/app_service/app_service.tf
+++ azure-web-app/app_service/app_service.tf
@@ -16,7 +16,7 @@ resource "azurerm_app_service" "webapp_app_service" {
     type = "AzureBlob"
     name = var.st_id
     access_key = var.st_access
-    mount_path = "/site/wwwroot"
+    mount_path = "/home/site/wwwroot"
   }
 
     depends_on = [
