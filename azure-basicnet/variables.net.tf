a13a5b13ac2e48beb8894720dc0f4be56ac2fa44
Stuart Ansell <stuart@ansell.pw>
2022-03-03 15:07:38 +0000

    I'll come back to NSG Rules. I can't figure out flattening a nested map.

--- azure-basicnet/variables.net.tf
+++ azure-basicnet/variables.net.tf
@@ -34,31 +34,31 @@ variable "vnet_subnets" {
             destination_address_prefix = "*"
           }
 
-          sql = {
-            name                       = "sql"
-            priority                   = 101
-            direction                  = "Inbound"
-            access                     = "Allow"
-            protocol                   = "Tcp"
-            source_port_range          = "*"
-            destination_port_range     = "1433"
-            source_address_prefix      = "SqlManagement"
-            destination_address_prefix = "192.168.2.0/24"
-          }
-
-          http = {
-            name                       = "http"
-            priority                   = 201
-            direction                  = "Inbound"
-            access                     = "Allow"
-            protocol                   = "Tcp"
-            source_port_range          = "*"
-            destination_port_range     = "80"
-            source_address_prefix      = "*"
-            destination_address_prefix = "192.168.2.0/24"
-          }
-        }
+      sql = {
+        name                       = "sql"
+        priority                   = 101
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "1433"
+        source_address_prefix      = "SqlManagement"
+        destination_address_prefix = "192.168.2.0/24"
       }
+
+      http = {
+        name                       = "http"
+        priority                   = 201
+        direction                  = "Inbound"
+        access                     = "Allow"
+        protocol                   = "Tcp"
+        source_port_range          = "*"
+        destination_port_range     = "80"
+        source_address_prefix      = "*"
+        destination_address_prefix = "192.168.2.0/24"
+      }
+    }
+    }
       vault = {
         name = "vault"
         addresses = [ "192.168.129.0/24" ]
